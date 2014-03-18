adfc25fe4f37d93f74ede711c716edbf12eda0a9
Vasili Abolmasov <ankatern@gmail.com>
2014-03-18 12:30:46 +0400

    Updatet route to static files.

--- PythonDjango/settings.py
+++ PythonDjango/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
     'authentication',
 )
 
@@ -87,3 +85,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#STATIC_ROOT = "/PythonDjango/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+)
